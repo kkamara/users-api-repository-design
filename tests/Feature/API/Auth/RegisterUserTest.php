79f6f23e8de07b5c3a8b899665da6d542fc52801
kkamara <kamaracomputers@gmail.com>
2025-03-06 01:13:04 +0000

    Add RegisterUserTest API feature tests

---
+++ tests/Feature/API/Auth/RegisterUserTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Feature\API\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegisterUserTest extends TestCase
+{
+    protected $headers = ['Content-Type' => 'application/json'];
+
+    /**
+     * A feature for the successful register response.
+     */
+    public function test_successful_register(): void
+    {
+        $userName = "New User";
+        $userEmail = "newuser12111@doe.com";
+        $userPassword = config("testing.user_password");
+
+        $response = $this->withHeaders($this->headers)
+            ->postJson(
+                "/api/auth/register",
+                [
+                    "name" => $userName,
+                    "email" => $userEmail,
+                    "password" => $userPassword,
+                ],
+            );
+        $response->assertCreated();
+        $response->assertJsonFragment(["email" => $userEmail]);
+        $response->assertJsonStructure([
+            "data" => [
+                "id",
+                "name",
+                "email",
+                "createdAt",
+                "updatedAt",
+            ],
+        ]);
+    }
+
+    /**
+     * A feature for the validation error response.
+     */
+    public function test_validation_error(): void
+    {
+        $userEmail = "newuser12111@doe.com";
+        $userPassword = config("testing.user_password");
+
+        $response = $this->withHeaders($this->headers)
+            ->postJson(
+                "/api/auth/register",
+                [
+                    "email" => $userEmail,
+                    "password" => $userPassword,
+                ],
+            );
+        $response->assertBadRequest();
+        $response->assertJsonFragment([
+            "message" => "The name field is required.",
+        ]);
+    }
+}
