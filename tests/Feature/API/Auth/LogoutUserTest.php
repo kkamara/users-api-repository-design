3bc2eac98bea2b6413313834ae5abdc0a5bb5a14
kkamara <kamaracomputers@gmail.com>
2025-03-06 01:00:40 +0000

    Add LogoutUserTest API feature test

---
+++ tests/Feature/API/Auth/LogoutUserTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature\API\Auth;
+
+use App\Models\User;
+use Exception;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
+use Tests\TestCase;
+use Laravel\Sanctum\Sanctum;
+
+class LogoutUserTest extends TestCase
+{
+    protected $headers = ['Content-Type' => 'application/json'];
+
+    /**
+     * A feature test for the successful logout response.
+     */
+    public function test_successful_logout(): void
+    {
+        $this->seed();
+        
+        $userEmail = config("testing.user_email");
+
+        $user = User::where("email", $userEmail)
+            ->firstOrFail();
+
+        Sanctum::actingAs($user);
+
+        $response = $this->withHeaders($this->headers)
+            ->deleteJson('/api/auth');
+        $response->assertStatus(Response::HTTP_OK);
+    }
+}
